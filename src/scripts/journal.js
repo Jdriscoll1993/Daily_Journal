82f47827f37b85a286fd1da2e712b8a5ab1f2d38
Joey Driscoll <driscoll.recording@gmail.com>
2019-04-12 10:26:28 -0500

    Journal Entry exercisers 1 & 2

---
+++ src/scripts/journal.js
@@ -0,0 +1,25 @@
+console.log('journal.js');
+const entries = []
+const objectsEntry = {
+    date: "4.11.2019",
+    concept: "Introduced objects",
+    content: "Took a dive into objects, arrays, and the differences betwen them",
+    mood: "default",
+    id: 1
+}
+const loopsEntry = {
+    date: "4.11.2019",
+    concept: "Experimented with different types of loops",
+    content: "For loops, For Each loops--down the road we'll be looking into other types of loops.",
+    mood: "default",
+    id: 2
+}
+const functionsEntry = {
+    date: "4.11.2019",
+    concept: "Introduced functions",
+    content: "Experimented with creating functions and calling them.",
+    mood: "default",
+    id: 3
+}
+entries.push(objectsEntry, loopsEntry);
+console.log(entries);
